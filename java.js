9190c1803b1d60c3c555e8ad49c084823d0af4ce
Iklash khan <111762117+ik5184@users.noreply.github.com>
2023-06-19 23:29:57 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,9 @@
+const calcy=() =>{
+    let wd= document.getElementById('web').value;
+    let maths= document.getElementById('math').value;
+    let phy= sdocument.getElementById('physics').value;
+    let chem = document.getElementById('chemistry').value;
+    let total=wd+maths+phy+chem;
+   
+    
+}
